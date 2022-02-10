72fe0b11bf409c81f8610ada708fc5de645951b4
Jaswal1p <Jaswal1p@yahoo.com>
2022-02-10 16:17:52 -0500

    attempted connection to the mySQL

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,18 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'jaswal1p',
+        // Your MySQL password
+        password: 'nahcYj-6fegwo-pifbyt',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 
