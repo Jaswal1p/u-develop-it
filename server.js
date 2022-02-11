2dcb35df4989885413203b992b1941fd092888d5
Jaswal1p <Jaswal1p@yahoo.com>
2022-02-11 00:56:33 -0500

    coded for deleteing candidate route & tested with insomnia

--- server.js
+++ server.js
@@ -22,30 +22,74 @@ const db = mysql.createConnection(
   );
 
 
-// Default response for any othere request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-});
+  // app.get('/', (req, res) => {
+  //   res.json({
+  //     message: 'Hello World'
+  //   });
+  // });
+  
 
 
 // Get all candidates
-// db.query(`SELECT * FROM candidates`, (err,rows) => {
-//     console.log(rows);
+// app.get('/api/candidates', (req, res) => {
+//   const sql = `SELECT * FROM candidates`;
+
+//   db.query(sql, (err, rows) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: 'success',
+//       data: rows
+//     });
+//   });
 // });
 
 
 
-// GET a single candidate
-// db.query(`SELECT * FROM candidates WHERE id = 3`, (err, row) => {
+
+// Get a single candidate
+// app.get('/api/candidate/:id', (req, res) => {
+//   const sql = `SELECT * FROM candidates WHERE id = ?`;
+//   const params = [req.params.id];
+
+//   db.query(sql, params, (err, row) => {
 //     if (err) {
-//         console.log(err);
+//       res.status(400).json({ error: err.message });
+//       return;
 //     }
-//     console.log(row);
+//     res.json({
+//       message: 'success',
+//       data: row
+//     });
+//   });
 // });
 
 
 
 // Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
 // db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
 //     if (err) {
 //         console.log(err);
@@ -66,6 +110,15 @@ app.use((req, res) => {
 //     console.log(result);
 // });
 
+
+
+
+
+
+// Default response for any othere request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
 
 
 app.listen(PORT, () => {
